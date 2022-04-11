a97ee64bae1bbb26596194438060b7e2a287cdb2
Jay035 <jaydencolins08@gmail.com>
2022-04-11 21:23:56 +0100

    update

--- docs/app.js
+++ docs/app.js
@@ -2,5 +2,5 @@ const fetchAdvice = async () => {
     const response = await fetch('https://api.adviceslip.com/advice')
     const data = await response.json();
     document.querySelector('.id').textContent = data.slip.id;
-    document.querySelector('.quote').textContent = data.slip.advice;
+    document.querySelector('.quote').innerHTML = `"${data.slip.advice}"`;
 }
