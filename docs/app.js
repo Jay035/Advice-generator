78059215aa230b853bbaedf31dc38e61196473eb
Jay035 <jaydencolins08@gmail.com>
2022-04-12 11:03:56 +0100

    update on animation

--- docs/app.js
+++ docs/app.js
@@ -3,4 +3,5 @@ const fetchAdvice = async () => {
     const data = await response.json();
     document.querySelector('.id').textContent = data.slip.id;
     document.querySelector('.quote').innerHTML = `"${data.slip.advice}"`;
+    document.querySelector('.quote').classList.add('animate')
 }
